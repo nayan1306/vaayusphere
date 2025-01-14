07e7a3e46ba5574841fc4d5412795e7b90dd8559
nayan1306 <vnayan1306@gmail.com>
2025-01-14 16:36:51 +0530

    weather card main component finalized

--- lib/Api/fetchweather.dart
+++ lib/Api/fetchweather.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 Future<Map<String, dynamic>> fetchWeatherData() async {
   const url =
-      "https://api.open-meteo.com/v1/forecast?latitude=21.2333&longitude=81.6333&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,visibility,wind_speed_10m,is_day&timezone=auto&past_hours=24&forecast_hours=1";
+      "https://api.open-meteo.com/v1/forecast?latitude=21.2333&longitude=81.6333&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,visibility,wind_speed_10m,is_day&daily=weather_code&timezone=auto&past_hours=24&forecast_hours=1";
   try {
     final response = await http.get(Uri.parse(url));
     // log("message: ${response.body}");
--- lib/widgets/weathercard.dart
+++ lib/widgets/weathercard.dart
@@ -27,24 +27,28 @@ class WeatherCard extends StatelessWidget {
           children: [
             // Weather Info
             weatherData != null
-                ? Column(
+                ? Row(
                     children: [
-                      // Temperature
-                      Row(
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          if (weatherData['hourly']['is_day'][0] == 1)
-                            Image.network(
-                              "https://raw.githubusercontent.com/nayan1306/assets/main/sun.png",
-                              width: 100,
-                              loadingBuilder:
-                                  (context, child, loadingProgress) {
-                                if (loadingProgress == null) {
-                                  return child;
-                                } else {
-                                  return Center(
-                                    child: CircularProgressIndicator(
-                                      value:
-                                          loadingProgress.expectedTotalBytes !=
+                          // Temperature
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              if (weatherData['hourly']['is_day'][0] == 1)
+                                Image.network(
+                                  "https://raw.githubusercontent.com/nayan1306/assets/main/sun.png",
+                                  width: 100,
+                                  loadingBuilder:
+                                      (context, child, loadingProgress) {
+                                    if (loadingProgress == null) {
+                                      return child;
+                                    } else {
+                                      return Center(
+                                        child: CircularProgressIndicator(
+                                          value: loadingProgress
+                                                      .expectedTotalBytes !=
                                                   null
                                               ? loadingProgress
                                                       .cumulativeBytesLoaded /
@@ -52,113 +56,133 @@ class WeatherCard extends StatelessWidget {
                                                           .expectedTotalBytes ??
                                                       1)
                                               : null,
-                                    ),
-                                  );
-                                }
-                              },
-                            )
-                          else
-                            const Icon(
-                              Icons.nightlight_round,
-                              color: Colors.white70,
-                              size: 80,
-                            ),
-                          const SizedBox(width: 10),
-                          Text(
-                            "${weatherData['hourly']['temperature_2m'][0]}°C", // Display the first temperature reading
-                            style: const TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
+                                        ),
+                                      );
+                                    }
+                                  },
+                                )
+                              else
+                                const Icon(
+                                  Icons.nightlight_round,
+                                  color: Colors.white70,
+                                  size: 100,
+                                ),
+                              const SizedBox(width: 10),
+                              Text(
+                                "${weatherData['hourly']['temperature_2m'][0]}°C", // Display the first temperature reading
+                                style: const TextStyle(
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // Wind Speed
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.waves,
+                                        color: Colors.white70,
+                                        size: 20,
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text(
+                                        "${weatherData['hourly']['wind_speed_10m'][0]} km/h", // Display wind speed
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white70,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
 
-                      // Wind Speed
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.waves,
-                            color: Colors.white70,
-                            size: 20,
-                          ),
-                          const SizedBox(width: 5),
-                          Text(
-                            "${weatherData['hourly']['wind_speed_10m'][0]} km/h", // Display wind speed
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.white70,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
+                                  // Humidity
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.cloud,
+                                        color: Colors.white70,
+                                        size: 20,
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text(
+                                        "${weatherData['hourly']['relative_humidity_2m'][0]}%", // Display humidity
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white70,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(width: 20),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // Precipitation Probability
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.water_drop,
+                                        color: Colors.white70,
+                                        size: 20,
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text(
+                                        "${weatherData['hourly']['precipitation_probability'][0]}%", // Display precipitation probability
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white70,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
 
-                      // Humidity
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.cloud,
-                            color: Colors.white70,
-                            size: 20,
+                                  // Visibility
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.visibility,
+                                        color: Colors.white70,
+                                        size: 20,
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text(
+                                        "${weatherData['hourly']['visibility'][0]} m", // Display visibility
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white70,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                          const SizedBox(width: 5),
-                          Text(
-                            "${weatherData['hourly']['relative_humidity_2m'][0]}%", // Display humidity
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.white70,
-                            ),
-                          ),
+                          const SizedBox(
+                            height: 40,
+                            width: 290,
+                          )
                         ],
                       ),
-                      const SizedBox(height: 10),
-
-                      // Precipitation Probability
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.water_drop,
-                            color: Colors.white70,
-                            size: 20,
-                          ),
-                          const SizedBox(width: 5),
-                          Text(
-                            "${weatherData['hourly']['precipitation_probability'][0]}%", // Display precipitation probability
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.white70,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-
-                      // Visibility
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.visibility,
-                            color: Colors.white70,
-                            size: 20,
-                          ),
-                          const SizedBox(width: 5),
-                          Text(
-                            "${weatherData['hourly']['visibility'][0]} m", // Display visibility
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.white70,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
+                      // weather forecast
+                      Container(
+                        color: Colors.amber,
+                        height: 250,
+                        width: 500,
+                      )
                     ],
                   )
                 : SizedBox(
@@ -171,11 +195,8 @@ class WeatherCard extends StatelessWidget {
                         Colors.blue.shade400, // Cool, peaceful azure blue
                         Colors.blue.shade500, // Subtle and deeper blue
                       ],
-
                       indicatorType: Indicator.lineScale,
                       strokeWidth: 3,
-
-                      // pathBackgroundColor: Colors.black45,
                     ),
                   ),
           ],
